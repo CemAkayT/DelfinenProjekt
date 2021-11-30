26ba905c4a946aad88bdabf17935b25dedd63419
cemakay <cema0092@stud.kea.dk>
2021-11-30 10:16:00 +0100

    Result klasse færdiggjort plus underklasser.

--- src/com/company/domain/CompetitionResult.java
+++ src/com/company/domain/CompetitionResult.java
@@ -1,4 +1,10 @@
 package com.company.domain;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
 public class CompetitionResult extends Result{
+    public CompetitionResult(double resultTime, UUID idNum, LocalDate dateOfReult) {
+        super(resultTime, idNum, dateOfReult);
+    }
 }
--- src/com/company/domain/TrainingResult.java
+++ src/com/company/domain/TrainingResult.java
@@ -1,4 +1,10 @@
 package com.company.domain;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
 public class TrainingResult extends Result{
+    public TrainingResult(double resultTime, UUID idNum, LocalDate dateOfReult) {
+        super(resultTime, idNum, dateOfReult);
+    }
 }
